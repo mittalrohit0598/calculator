271d1dc5c1acc2fb62d8bf748e9656896cf34bca
mittalrohit0598 <mittalrohit0598@gmail.com>
2020-08-27 11:46:11 +0530

    Make calculator work using mouse

--- scripts/main.js
+++ scripts/main.js
@@ -11,7 +11,11 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if(b){
+        return a / b
+    } else {
+        return 'REALLY?';
+    }
 }
 
 function operate(a, b, operator) {
@@ -22,5 +26,65 @@ function operate(a, b, operator) {
         case '*': res = multiply(a, b); break;
         case '/': res = divide(a, b); break;
     }
-    return(res);
+    return res == Math.floor(res) ? res : (res).toFixed(2);;
 }
+
+const display = document.querySelector('#display');
+const keys = Array.from(document.querySelectorAll('button'));
+const decimal = document.querySelector('#decimal');
+
+let firstOperand = NaN;
+let secondOperand = NaN;
+let operator = "";
+display.textContent = "";
+
+keys.forEach((key) => {
+    key.addEventListener('click', () => {
+        if(key.textContent == 'C'){
+            display.textContent = "";
+            decimal.disabled = false;
+            firstOperand = NaN;
+            secondOperand = NaN;
+            operator = "";
+        } else if(key.classList.contains('operators')){
+            if(Object.is(secondOperand, NaN) && operator == ""){
+                if(display.textContent !== ""){
+                    firstOperand = +display.textContent;
+                    operator = key.textContent;
+                    display.textContent += ` ${key.textContent} `;
+                    decimal.disabled = false;
+                }
+            } else if(!Object.is(firstOperand, NaN) && operator != "" && !display.textContent.slice(display.textContent.indexOf(operator) + 2, display.textContent.length) == 0){
+                secondOperand = +display.textContent.slice(display.textContent.indexOf(operator) + 2, display.textContent.length);
+                const result = operate(firstOperand, secondOperand, operator);
+                firstOperand = result;
+                operator = key.textContent;
+                secondOperand = NaN;
+                display.textContent = result + ` ${operator} `;
+                decimal.disabled = false;
+            } else if(!Object.is(firstOperand, NaN)){
+                operator = key.textContent;
+                display.textContent = `${firstOperand} ${operator} `;
+            }
+        } else if(key.textContent == '='){
+            if(!Object.is(firstOperand, NaN) && operator != "" && !display.textContent.slice(display.textContent.indexOf(operator) + 2, display.textContent.length) == 0){
+                secondOperand = +display.textContent.slice(display.textContent.indexOf(operator) + 2, display.textContent.length);
+                const result = operate(firstOperand, secondOperand, operator);
+                display.textContent = result
+                firstOperand = result;
+                operator = "";
+                secondOperand = NaN;
+                if(firstOperand != Math.floor(firstOperand)){
+                    decimal.disabled = true;
+                } else {
+                    decimal.disabled = false;
+                }
+            }
+        } else {
+            display.textContent += key.textContent;
+            if(key.textContent == '.'){
+                decimal.disabled = true;
+            }
+        }
+    });
+});
